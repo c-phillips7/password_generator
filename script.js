5a832f34bd86c69d662b17dfc4d7c463265338d7
Chris Phillips <cp.phillips15@gmail.com>
2021-10-18 20:57:40 -0400

    added array of possible characters, and some steps from demonstration in class

--- script.js
+++ script.js
@@ -1,8 +1,136 @@
-// Assignment Code
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Assignment Code 
+
 var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+
+}
+
+
+function getUserOptions() {
+
+  var userChoices = {};
+
+// ask for length
+  var userLength = prompt('how long would you like your password to be?')
+
+  // check if non numbers used (would return NaN)
+  parseInt(userLength)
+
+// sign to userOptions
+  userChoices.userLength = length;
+
+  // ask for UpperCase
+  var userUpperCase = confirm('Would you like UpperCase letters?')
+
+
+// sign to userOptions
+  userChoices.userUpperCase = UpperCase;
+
+  console.log(userChoices);
+}
+
+
+
+function getUserOptions() {
+
+  
+}
+
 
 // Write password to the #password input
 function writePassword() {
+
+  // get user optons
+  var userOptions = getUserOptions()
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +140,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
